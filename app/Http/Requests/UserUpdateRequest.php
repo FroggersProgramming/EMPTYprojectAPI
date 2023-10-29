ebe53c1aefbd53f7322bab4b0b32fe79d12532cf
Konstantin_Ermolaev <64197341+ThaHatty-KonstantinErmolaev@users.noreply.github.com>
2023-10-29 14:28:16 +0700

    Добавлен валидатор для обновления пользователя
    Добавлен валидатор для обновления пользователя.
    Поля:
    user.name,
    user.email,
    user.login,
    user.password,
    role_id.

---
+++ app/Http/Requests/UserUpdateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserUpdateRequest extends FormRequest
+{
+//    /**
+//     * Determine if the user is authorized to make this request.
+//     */
+//    public function authorize(): bool
+//    {
+//        return false;
+//    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user.name'  =>  'string|alpha',
+            'user.email' =>  'email|unique:users',
+            'user.login' =>  'string',
+            'user.password'  =>  'string|min:10|max:30',
+            'role_id'   =>  'exists:roles,id',
+        ];
+    }
+}
